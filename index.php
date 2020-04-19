f60fb2be0e9a34c48c72e1be448f17f492ce7c2a
wwsalmon <wwsamson@yahoo.com>
2020-04-19 00:21:31 -0400

    more parallax changes

--- index.php
+++ index.php
@@ -1,32 +1,34 @@
 <?php get_header(); ?>
 
-<div class='top-container' id='parallax-scene' data-tilt data-tilt-max="5">
-    <div class='banner-item' style="top: 70%; right: 30%;">
-        <div class="banner-item-back"></div>
-        <div class='banner-item-inner' style="transform: translateZ(60px);">
-            <div class='banner-item-left'><span>#1</span></div>
-            <div class='banner-item-right'>
-                <span class='banner-main'>Rated American<br />Female Chess Player<br /></span>
-                <span class='banner-small'>Since September 2019</span>
+<div class='top-container'>
+    <div class="top-container-inner" data-tilt data-tilt-max="5" data-tilt-full-page-listening>
+        <div class='banner-item' style="margin-left: 0px; margin-top: 300px;">
+            <div class="banner-item-back"></div>
+            <div class='banner-item-inner' style="transform: translateZ(60px);">
+                <div class='banner-item-left'><span>#1</span></div>
+                <div class='banner-item-right'>
+                    <span class='banner-main'>Rated American<br />Female Chess Player<br /></span>
+                    <span class='banner-small'>Since September 2019</span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class='banner-item' style="top: 30%; left: 30%;">
-        <div class="banner-item-back"></div>
-        <div class='banner-item-inner' style="transform: translateZ(40px);">
-            <div class='banner-item-left'><span>#1</span></div>
-            <div class='banner-item-right'>
-                <span class='banner-main'>Rated American<br />Female Chess Player<br /></span>
-                <span class='banner-small'>Since September 2019</span>
+        <div class='banner-item' style="margin-left: 100px; margin-top: 100px;">
+            <div class="banner-item-back"></div>
+            <div class='banner-item-inner' style="transform: translateZ(40px);">
+                <div class='banner-item-left'><span>#1</span></div>
+                <div class='banner-item-right'>
+                    <span class='banner-main'>Rated American<br />Female Chess Player<br /></span>
+                    <span class='banner-small'>Since September 2019</span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class='banner-item' style="top: 10%; right: 20%;">
-        <div class="banner-item-back"></div>
-        <div class='banner-item-inner' style="transform: translateZ(20px);">
-            <div class='banner-item-right'>
-                <span class='banner-main'>Youngest Female Ever to<br/>Beat a Grandmaster<br /></span>
-                <span class='banner-small'>August 2014 v. Ivanov, at the age of 10</span>
+        <div class='banner-item' style="margin-left: -100px; margin-top: -100px;">
+            <div class="banner-item-back"></div>
+            <div class='banner-item-inner' style="transform: translateZ(20px);">
+                <div class='banner-item-right'>
+                    <span class='banner-main'>Youngest Female Ever to<br/>Beat a Grandmaster<br /></span>
+                    <span class='banner-small'>August 2014 v. Ivanov, at the age of 10</span>
+                </div>
             </div>
         </div>
     </div>
